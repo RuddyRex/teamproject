3125c5ac09a5d25f95d2feb34c0fd07ba6bb78c9
Ali Rıza Yücefaydalı <39056822+alirzaycefydli@users.noreply.github.com>
2023-01-07 14:13:08 +0100

    initial commit

--- src/main/java/com/ruddyrex/teamproject/RuddyRex/App.java
+++ src/main/java/com/ruddyrex/teamproject/RuddyRex/App.java
@@ -1,35 +1,9 @@
 package com.ruddyrex.teamproject.RuddyRex;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLDataException;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class App {
   public static void main(String[] args) {
-	  ResultSet result = null;
-		
-		 try {
-				 Class.forName("org.hsqldb.jdbcDriver");
-				  
-				 Connection con = DriverManager.getConnection("jdbc:hsqldb:hsql//localhost/testdb", "SA", "");
 	
-				 Statement stmt = con.createStatement();
-				
-				 
-				result = stmt.executeQuery("CREATE TABLE older (id INT NOT NULL)");
-				 
-				 //ResultSet rs = stmt.executeQuery("select * from users");
-				 
-				 System.out.println(result);
-			  
-			 con.close();
-			  
-			 } catch (ClassNotFoundException | SQLException e) {
-			 e.printStackTrace();
-			 }
 	  
   }
 }
