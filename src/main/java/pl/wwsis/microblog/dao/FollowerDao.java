8d209e2a5c494d128db72aa764ba44e994cacbd1
CemArslan(229904) <CemArslan(229904)@DESKTOP-9K71BMB>
2023-01-23 12:43:04 +0100

    added methods to dao's

--- src/main/java/pl/wwsis/microblog/dao/FollowerDao.java
+++ src/main/java/pl/wwsis/microblog/dao/FollowerDao.java
@@ -1,5 +1,12 @@
 package pl.wwsis.microblog.dao;
 
-public interface FollowerDao {
+import java.util.List;
 
+import pl.wwsis.microblog.model.follower;
+
+
+public interface FollowerDao {
+	public void addFollower(int followerId, int followedId);
+	public void unFollow(int followerId, int unfollowedId);
+	public List<follower> checkingFollowerList(int followedId);
 }
--- src/main/java/pl/wwsis/microblog/dao/UserDao.java
+++ src/main/java/pl/wwsis/microblog/dao/UserDao.java
@@ -1,5 +1,7 @@
 package pl.wwsis.microblog.dao;
+import pl.wwsis.microblog.model.user;
 
 public interface UserDao {
-
+	public user getUserLogin(int id);
+	public void createUser(int id, String firstName,String last_name, String passw);
 }
