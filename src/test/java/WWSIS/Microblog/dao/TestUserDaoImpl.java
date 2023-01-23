b7cbcb7955b151e05184283f0009d4184608046d
CemArslan(229904) <CemArslan(229904)@DESKTOP-9K71BMB>
2023-01-23 14:46:09 +0100

    added test structure

---
+++ src/test/java/WWSIS/Microblog/dao/TestUserDaoImpl.java
@@ -0,0 +1,14 @@
+package WWSIS.Microblog.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestUserDaoImpl {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
