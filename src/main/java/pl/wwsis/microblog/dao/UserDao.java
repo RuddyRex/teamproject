ed1b21083291ef7b1b2c27e349ab9876161abd99
CemArslan(229904) <CemArslan(229904)@DESKTOP-9K71BMB>
2023-01-23 13:46:48 +0100

    added dao implementation methods

--- src/main/java/pl/wwsis/microblog/dao/UserDao.java
+++ src/main/java/pl/wwsis/microblog/dao/UserDao.java
@@ -3,5 +3,5 @@ import pl.wwsis.microblog.model.user;
 
 public interface UserDao {
 	public user getUserLogin(int id);
-	public void createUser(int id, String firstName,String last_name, String passw);
+	public void createUser(user User);
 }
