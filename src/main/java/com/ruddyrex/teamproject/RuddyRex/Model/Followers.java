d0743a101d36b8eaf789c958b7eeaaff2ea4dada
Ali Rıza Yücefaydalı <39056822+alirzaycefydli@users.noreply.github.com>
2023-01-26 13:12:58 +0100

    Task 5: Model created

---
+++ src/main/java/com/ruddyrex/teamproject/RuddyRex/Model/Followers.java
@@ -0,0 +1,56 @@
+package com.ruddyrex.teamproject.RuddyRex.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="followers")
+public class Followers {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id", nullable=false)
+	private Long id;
+	
+	@OneToOne
+	@JoinColumn(name = "id")
+	@Column(name = "follower_id")
+	private User follower;
+	
+	@OneToOne
+	@JoinColumn(name = "id")
+	@Column(name = "followee_id")
+	private User followee;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getFollower() {
+		return follower;
+	}
+
+	public void setFollower(User follower) {
+		this.follower = follower;
+	}
+
+	public User getFollowee() {
+		return followee;
+	}
+
+	public void setFollowee(User followee) {
+		this.followee = followee;
+	}
+	
+	
+}
