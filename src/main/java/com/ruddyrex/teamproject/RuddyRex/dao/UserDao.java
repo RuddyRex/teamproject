6e997818a2389a4b69691826d30ac9046ff366d5
luisernestopoland <71884438+luisernestopoland@users.noreply.github.com>
2023-01-31 18:52:02 +0100

    Revert "final pr"

--- src/main/java/com/ruddyrex/teamproject/RuddyRex/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.ruddyrex.teamproject.RuddyRex.dao;
-
-import com.ruddyrex.teamproject.RuddyRex.Model.User;
-
-public interface UserDao{
-	public User findUserById(int id);
-	public User findUserByNickname(String nickname);
-	public void createUser(User User);
-}
